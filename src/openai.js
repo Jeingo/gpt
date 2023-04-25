36db6db8b294197a9d91b8c2debf20ffc4b613fe
Renat <jeingo20@gmail.com>
2023-04-25 22:31:32 +0300

    init

---
+++ src/openai.js
@@ -0,0 +1,52 @@
+import { Configuration, OpenAIApi } from 'openai'
+import config from "config";
+import { createReadStream } from 'fs'
+
+class OpenAI {
+    roles = {
+        ASSISTANT: 'assistant',
+        USER: 'user',
+        SYSTEM: 'system'
+    }
+
+    constructor(apiKey) {
+        const configuration = new Configuration({
+            apiKey: apiKey,
+        });
+        this.openai = new OpenAIApi(configuration);
+    }
+
+    async chat(messages) {
+        try {
+            const response = await this.openai.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages
+            })
+            return response.data.choices[0].message
+        } catch (e) {
+            console.log(`Error while chat ${e.message}`)
+        }
+    }
+
+    async transcription(filepath) {
+        try {
+            const response = await this.openai.createTranscription(
+                createReadStream(filepath),
+                'whisper-1'
+            )
+            return response.data.text
+        } catch (e) {
+            console.log(`Error while transcription ${e.message}`)
+        }
+    }
+}
+
+export const openai = new OpenAI(config.get('OPENAI_KEY'))
+
+// const response2 = await this.openai.createImage({
+//     prompt: "a white siamese cat",
+//     n: 1,
+//     size: "1024x1024",
+// });
+// const image_url = response2.data.data[0].url;
+// console.log(image_url)
